db9f68fe0d26028e585a0b376c4e10f2ffd9fe77
HarshitaJoshi04 <hjoshikspuk04@gmail.com>
2025-07-15 12:58:11 +0530

    correct fetch urls

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function formatTime(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`http://127.0.0.1:5500/${folder}/`)
+    let a = await fetch(`/${folder}/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -76,7 +76,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`http://127.0.0.1:5500/songs/`)
+    let a = await fetch(`/songs/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -91,7 +91,7 @@ async function displayAlbums() {
             // console.log("folder = ", folder);
 
             // get the metadata of the folder
-            let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`)
+            let a = await fetch(`/songs/${folder}/info.json`)
             let response = await a.json();
 
             cardContainer.innerHTML = cardContainer.innerHTML + ` <div  data-folder="${folder}" class="card">
